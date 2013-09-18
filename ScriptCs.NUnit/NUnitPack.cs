06efbb143ed1527d80a17fca33f1e215f2d342c6
Filip W <wojcieszyn@gmail.com>
2013-09-18 20:55:33 +0200

    Updated to remove unnecessary DLL import

--- ScriptCs.NUnit/NUnitPack.cs
+++ ScriptCs.NUnit/NUnitPack.cs
@@ -6,15 +6,8 @@ namespace ScriptCs.NUnit
     {
         public void Initialize(IScriptPackSession session)
         {
-            session.AddReference("nunit.core.dll");
-            session.AddReference("nunit.core.interfaces.dll");
-            session.AddReference("nunit.framework.dll");
-
             session.ImportNamespace("NUnit.Framework");
             session.ImportNamespace("ScriptCs.NUnit");
-            session.ImportNamespace("ScriptCs.NUnit");
-            session.ImportNamespace("System");
-            session.ImportNamespace("System.Linq");
             session.ImportNamespace("System.Reflection");
         }
 
